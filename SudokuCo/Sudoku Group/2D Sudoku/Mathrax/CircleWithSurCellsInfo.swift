c780a5dc82556cf47bc435be561aaf4b53a9054f
Anastasia <goryachevskaya.nastya@yandex.ru>
2022-08-13 00:50:43 +0200

    fix - correct displaying circles in Mathrax with notes

--- SudokuCo/Sudoku Group/2D Sudoku/Mathrax/CircleWithSurCellsInfo.swift
+++ SudokuCo/Sudoku Group/2D Sudoku/Mathrax/CircleWithSurCellsInfo.swift
@@ -21,7 +21,7 @@ class CircleWithSurCellsInfo: UIView {
     let types: [CircleType: String] = [.multiply: "*", .difference: "-", .division: "/", .sum: "+", .even: "E", .odd: "O"]
     
     func configureCircle(cellSize: CGFloat, circleType: CircleType, value: String) {
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: 0, y: 0), radius: CGFloat(cellSize / 3), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: 0, y: 0), radius: CGFloat(cellSize / 4), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
         
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = circlePath.cgPath
@@ -35,7 +35,7 @@ class CircleWithSurCellsInfo: UIView {
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: cellSize, height: cellSize))
         label.center = self.center
         label.textAlignment = .center
-        label.font = .systemFont(ofSize: 14)
+        label.font = .systemFont(ofSize: 12)
         label.text = value + types[circleType]!
         self.addSubview(label)
     }
