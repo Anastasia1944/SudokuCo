4490d5fe0a25ffd2ffe7845d2298d16a313f8c42
Anastasia <goryachevskaya.nastya@yandex.ru>
2022-10-04 13:31:38 +0200

    change ui in game settings

--- SudokuCo/Settings/SettingsViewController.swift
+++ SudokuCo/Settings/SettingsViewController.swift
@@ -18,19 +18,32 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .whiteSys
+        self.view.backgroundColor = .beige
         
         settignsTableView.dataSource = self
         settignsTableView.delegate = self
         
+        navBarSettings()
         configureTableView()
+    }
+    
+    func navBarSettings() {
+        navigationItem.title = "Settings"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.lightBlue, .font: UIFont.systemFont(ofSize: 24)]
+        navigationController?.navigationBar.barTintColor = .beige
+        
+        let backButton = UIBarButtonItem()
+        backButton.title = ""
+        backButton.tintColor = .lightBlue
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
     }
     
     func configureTableView() {
         view.addSubview(settignsTableView)
         
         settignsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "SettingsCell")
-        settignsTableView.rowHeight = 60
+        settignsTableView.rowHeight = 80
+        settignsTableView.backgroundColor = .clear
         
         settignsTableView.translatesAutoresizingMaskIntoConstraints = false
         settignsTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
@@ -52,13 +65,18 @@ extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
         
+        cell.backgroundColor = .clear
+        
         let switchView = UISwitch(frame: .zero)
         switchView.setOn(defaults.bool(forKey: settings[indexPath.row]), animated: true)
         switchView.tag = indexPath.row
+        switchView.onTintColor = .lightBlue
+        switchView.thumbTintColor = .beige
         switchView.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
         cell.accessoryView = switchView
         
         cell.textLabel?.text = settings[indexPath.row]
+        cell.textLabel?.textColor = .lightBlue
         
         return cell
     }
